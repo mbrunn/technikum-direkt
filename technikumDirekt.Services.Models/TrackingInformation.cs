d8bd002fcfdd55a90a993ab6116b1354ae30fd91
Michael <nixmix800@gmail.com>
2020-09-25 17:03:25 +0200

    added gitignore, controller

--- technikumDirekt.Services.Models/TrackingInformation.cs
+++ technikumDirekt.Services.Models/TrackingInformation.cs
@@ -47,7 +47,8 @@ namespace TechnikumDirekt.Services.Models
             /// Enum DeliveredEnum for Delivered
             /// </summary>
             [EnumMember(Value = "Delivered")]
-            DeliveredEnum = 2        }
+            DeliveredEnum = 2        
+        }
 
         /// <summary>
         /// State of the parcel.
@@ -72,105 +73,5 @@ namespace TechnikumDirekt.Services.Models
         [Required]
         [DataMember(Name="futureHops")]
         public List<HopArrival> FutureHops { get; set; }
-
-        /// <summary>
-        /// Returns the string presentation of the object
-        /// </summary>
-        /// <returns>String presentation of the object</returns>
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            sb.Append("class TrackingInformation {\n");
-            sb.Append("  State: ").Append(State).Append("\n");
-            sb.Append("  VisitedHops: ").Append(VisitedHops).Append("\n");
-            sb.Append("  FutureHops: ").Append(FutureHops).Append("\n");
-            sb.Append("}\n");
-            return sb.ToString();
-        }
-
-        /// <summary>
-        /// Returns the JSON string presentation of the object
-        /// </summary>
-        /// <returns>JSON string presentation of the object</returns>
-        public string ToJson()
-        {
-            return JsonConvert.SerializeObject(this, Formatting.Indented);
-        }
-
-        /// <summary>
-        /// Returns true if objects are equal
-        /// </summary>
-        /// <param name="obj">Object to be compared</param>
-        /// <returns>Boolean</returns>
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            return obj.GetType() == GetType() && Equals((TrackingInformation)obj);
-        }
-
-        /// <summary>
-        /// Returns true if TrackingInformation instances are equal
-        /// </summary>
-        /// <param name="other">Instance of TrackingInformation to be compared</param>
-        /// <returns>Boolean</returns>
-        public bool Equals(TrackingInformation other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-
-            return 
-                (
-                    State == other.State ||
-                    State != null &&
-                    State.Equals(other.State)
-                ) && 
-                (
-                    VisitedHops == other.VisitedHops ||
-                    VisitedHops != null &&
-                    VisitedHops.SequenceEqual(other.VisitedHops)
-                ) && 
-                (
-                    FutureHops == other.FutureHops ||
-                    FutureHops != null &&
-                    FutureHops.SequenceEqual(other.FutureHops)
-                );
-        }
-
-        /// <summary>
-        /// Gets the hash code
-        /// </summary>
-        /// <returns>Hash code</returns>
-        public override int GetHashCode()
-        {
-            unchecked // Overflow is fine, just wrap
-            {
-                var hashCode = 41;
-                // Suitable nullity checks etc, of course :)
-                    if (State != null)
-                    hashCode = hashCode * 59 + State.GetHashCode();
-                    if (VisitedHops != null)
-                    hashCode = hashCode * 59 + VisitedHops.GetHashCode();
-                    if (FutureHops != null)
-                    hashCode = hashCode * 59 + FutureHops.GetHashCode();
-                return hashCode;
-            }
-        }
-
-        #region Operators
-        #pragma warning disable 1591
-
-        public static bool operator ==(TrackingInformation left, TrackingInformation right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(TrackingInformation left, TrackingInformation right)
-        {
-            return !Equals(left, right);
-        }
-
-        #pragma warning restore 1591
-        #endregion Operators
     }
 }
