79d8d62bf12bb2be988da746a4272435196f8883
Michael <nixmix800@gmail.com>
2020-10-02 15:46:46 +0200

    changed project type to classlib with netstandard 2.1 (mr. knor)

--- TechnikumDirekt.Services.Models/TrackingInformation.cs
+++ TechnikumDirekt.Services.Models/TrackingInformation.cs
@@ -11,7 +11,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace TechnikumDirekt.Services.Models
 {
@@ -24,7 +24,7 @@ namespace TechnikumDirekt.Services.Models
         ///     State of the parcel.
         /// </summary>
         /// <value>State of the parcel.</value>
-        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
         public enum StateEnum
         {
             /// <summary>
